2a07e32249371caae9923798d5adf24ec21d47f2
shashankrp <shashankrp2@gmail.com>
2025-03-29 16:40:07 +0530

    made a small css change in home page

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       {/* <div className="container-md text-break pt-35 d-flex align-items-center content-center flex-row w-50"> */}
-      <div className="container-md text-break pt-35 d-flex align-items-center content-center flex-md-row flex-column w-50">
+      <div className="container-md text-break pt-35 d-flex align-items-center content-center flex-md-row flex-column w-auto space-y-5">
               <Image className='rounded mx-auto d-block h-2/5 w-2/5' src={image} alt="background" />
               <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
               <p className="pl-4 w-5/6 break-words">Hi my name is <b>Shashank</b> and am a <b>Software engineer</b> with <b>3+ years</b> of experience and currently working in <b>Ellucian.</b></p>
