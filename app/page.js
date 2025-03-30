b15e8249cad5a9e901dda8e4d75b6464e4ac643f
shashankrp <shashankrp2@gmail.com>
2025-03-30 15:52:44 +0530

    refactor: enhance header layout and improve styling on the home page

--- app/page.js
+++ app/page.js
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <div>
       {/* <div className="container-md text-break pt-35 d-flex align-items-center content-center flex-row w-50"> */}
-      <div className="container-md text-break pt-md-5 d-flex align-items-center content-center flex-md-row flex-column w-auto">
-              <Image className='rounded shadow mx-auto d-block img-fluid' src={image} alt="background" />
+      <div className="container-md text-break pt-md-5 d-flex align-items-center content-center flex-md-row flex-column w-auto p-5">
+              <Image className='rounded shadow mx-auto d-block img-fluid mb-4' src={image} alt="background" />
               <div className="d-flex flex-md-column justify-center w-auto px-md-5 row" >
-              <p className="pl-4 w-5/6 break-words fs-4">Hi my name is <b>Shashank</b> and am a <b>Software engineer</b> with <b>3+ years</b> of experience and currently working in <b>Ellucian.</b></p>
-              <a className="btn btn-primary" href="/skills">Know More</a>
+              <p className="pl-4 w-5/6 break-words fs-4" style={{textAlign: 'justify'}}>Hi my name is <b>Shashank</b> and am a <b>Software engineer</b> with <b>3+ years</b> of experience and currently working in <b>Ellucian.</b></p>
+              <a className="btn btn-primary" style={{alignSelf: "center"}} href="/skills">Know More</a>
               </div>
             </div>
     </div>
