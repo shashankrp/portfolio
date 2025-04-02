066a130e9aa6fbcccaf070bb7d628392da2c015d
shashankrp <shashankrp2@gmail.com>
2025-04-02 16:50:50 +0530

    fix: update header styles for improved responsiveness and consistency

--- app/headers.jsx
+++ app/headers.jsx
@@ -6,13 +6,13 @@ function Headers(props) {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid header-nav">
-                <a className="navbar-brand" href="/">Shashank RP</a>
+                <a className="navbar-brand font-monospace" href="/">Shashank RP</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 {/* Nav Section */}
                 <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-                    <ul className="navbar-nav px-45 px-md-0 justify-content-between flex-md-row">
+                    <ul className="navbar-nav px-45 px-md-0 justify-content-between flex-md-row d-none d-lg-inline-flex">
                         <li className="nav-item px-2">
                             <a className="nav-link active" aria-current="page1" href="./">Home</a>
                         </li>
@@ -33,20 +33,37 @@ function Headers(props) {
                         </li>
                     </ul>
 
-                    <ul className="nav justify-content-end flex-row-reverse d-lg-none">
-                    <li className="nav-item">
-                        <a className="nav-link active" target='_blank' aria-current="page" href="https://github.com/shashankrp"><i className="bi bi-github text-white"></i></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" target='_blank' href="https://www.linkedin.com/in/shashank-r-p-5a866b15a/"><i className='bi bi-linkedin text-white'></i></a>
-                    </li>
-                </ul>
+                    <ul className="navbar-nav px-45 px-md-0 justify-content-between flex-md-row d-lg-none">
+                        <li className="nav-item px-2">
+                            <a className="nav-link active" aria-current="page1" href="./">Home</a>
+                        </li>
+                        {/* <li className="nav-item">
+                            <a className="nav-link active" aria-current="page2" href="/about">Blog</a>
+                        </li> */}
+                        <li className="nav-item px-2">
+                            <a className="nav-link active" aria-current="page3" href="./skills">Skills</a>
+                        </li>
+                        <li className="nav-item px-2">
+                            <a className="nav-link active" aria-current="page4" href="./experience">Experience</a>
+                        </li>
+                        <li className="nav-item px-2">
+                            <a className="nav-link active" aria-current="page5" href="./contact">Contact</a>
+                        </li>
+                        <li className="nav-item px-2">
+                            <a className="nav-link active" aria-current="page6" href="./about">About</a>
+                        </li>
+                        <li className="nav-item px-2">
+                            <a className="nav-link d-flex font-monospace active" target='_blank' href="https://drive.google.com/file/d/1gzb3m_BdrhNwRfRFuF7IblGXIHCnRZip/view?usp=sharing">
+                                Resume &nbsp;<i className='bi bi-download align-content-end' style={{paddingLeft: "13rem"}}></i>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
                 {/* Footer Section */}
                 <ul className="nav justify-content-end flex-row-reverse d-none d-lg-inline-flex">
                     <li className="nav-item">
                         <a className="nav-link d-flex" target='_blank' href="https://drive.google.com/file/d/1gzb3m_BdrhNwRfRFuF7IblGXIHCnRZip/view?usp=sharing">
-                            <i className='bi bi-paperclip'></i><p className='fw-normal text-white h6 pt-1'>Resume</p>
+                            <i className='bi bi-download text-white'></i>&nbsp;&nbsp;<p className='fw-normal font-monospace text-white h6 pt-1'>Resume</p>
                         </a>
                     </li>
                 </ul>
