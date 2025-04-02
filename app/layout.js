6af5707203819b8d131338054fe6da291268a816
shashankrp <shashankrp2@gmail.com>
2025-04-02 16:09:05 +0530

    fix: update global styles and layout for improved background image handling

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import "./globals.css";
 import Headers from "./headers";
 import Script from 'next/script'
 import "bootstrap-icons/font/bootstrap-icons.css";
-import image from '../public/assets/images/background.jpg'
 
 export const metadata = {
   title: "Create Next App",
@@ -17,7 +16,7 @@ export default function RootLayout({ children }) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"></link>
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></Script>
       </head>
-      <body style={{backgroundImage: `url(${image.src})`, backgroundSize: 'inherit', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', height: '100vh'}}> 
+      <body> 
         <Headers />
         {children}
       </body>
