84f7d94829cb18c6d3db5d94684fb856a29ac593
shashankrp <shashankrp2@gmail.com>
2025-04-04 16:10:40 +0530

    feat: add blog page and update navigation links for improved accessibility

---
+++ app/blog/page.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='container my-container'>
+        <h1>Technical Blogs</h1>
+        <div>
+            <div className='row pt-4'>
+                        <div className="col">
+                            <em><h3>Ghibli-style image</h3></em>
+                        </div> 
+                        <div className="col text-end">
+                            <strong><em>04-Apr-2025</em></strong>
+                        </div>
+                    </div>
+            <p className='pt-4 text-justify'>
+                ChatGPT's is one of the most commonly used AI tool in the world and it is
+                used by many people for many purposes. It is used for writing, coding,
+                generating images, and many more. It is a great tool for people who are
+                looking for help in their work. For example, if you are a student and you are
+                looking for help in your assignments, you can use ChatGPT to get help.
+            </p>
+            <p className='pt-2 text-justify'>
+                If this technology is used in a good way, it can be very helpful for people.
+                But if it is used in a bad way, it can be very dangerous. As its a peace 
+                of code that is written by a human, if someone gains a control over the 
+                data that could be provided by you to the AI they could exactly predict
+                what you are doing or what you are thinking.
+            </p>
+            <p className='pt-2 text-justify'>ChatGPT's native image generation capabilities were introduced since 
+                the last update but people have become soo obsessed with one of its 
+                style which is <mark>Studio Ghibli-style images</mark> and they are 
+                enjyoing it a lot, but they are forgetting that its an AI. As AI are 
+                the kind of technology that learns from the inputs provided to it, 
+                so as of now it may not affect the people but in the future it may 
+                affect the people a lot, as it may use your pictures to genereate some
+                new images and it may be used in any criminal activities. Even though 
+                as the technologies are made to help people we should know what we 
+                should be sharing. So, be careful while using it. 
+            </p>
+            <p className='pt-4 text-justify'>
+                Final thoughts, I would say that ChatGPT is a great tool for people 
+                who are looking for help in their work. But we should be careful 
+                while using it. We should not share our personal information with 
+                it.    
+            </p>
+        </div>
+    </div>
+  )
+}
