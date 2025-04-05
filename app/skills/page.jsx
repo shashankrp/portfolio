3dd0435ab3061cde11bc8036d60a4db8232c6d07
shashankrp <shashankrp2@gmail.com>
2025-04-05 13:12:01 +0530

    refactor: update skills section with new technologies and progress percentages

--- app/skills/page.jsx
+++ app/skills/page.jsx
@@ -5,49 +5,40 @@ function page() {
         <div className='container-md text-wrap font-monospace pb-12 border-radius-md w-md font-bold my-container h-auto skills-container'>
             <h1>Skills</h1>
             <div className='py-10'>
-            {/* Next JS */}
+             {/* React JS */}
             <div className='place-self-auto pt-4 pb-xl-5 pb-4'>
                 <div className='col'>
-                    <p>Next JS</p>
-                </div>
-                <div className="progress w-auto" role="progressbar" aria-label="Success striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                    <div className="progress-bar progress-bar-striped bg-success" style={{width: "70%"}}><strong><em>70%</em></strong></div>
-                </div>
-            </div>
-                {/* React JS */}
-            <div className='place-self-auto pb-xl-5 pb-4'>
-                <div className='col'>
-                    <p>React JS</p>
+                    <p>React JS - V19</p>
                 </div>
                 <div className="progress w-auto" role="progressbar" aria-label="Success striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
                     <div className="progress-bar progress-bar-striped bg-success" style={{width: "80%"}}><strong><em>80%</em></strong></div>
                 </div>
             </div>
-            {/* Node JS */}
+            {/* AWS */}
             <div className='place-self-auto pb-xl-5 pb-4'>
                 <div className='col'>
-                    <p>Node JS</p>
+                    <p>AWS</p>
                 </div>
                 <div className="progress w-auto" role="progressbar" aria-label="Success striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
                     <div className="progress-bar progress-bar-striped bg-success" style={{width: "40%"}}><strong><em>40%</em></strong></div>
                 </div>
             </div>
-            {/* Java */}
+            {/* GIT */}
             <div className='place-self-auto pb-xl-5 pb-4'>
                 <div className='col'>
-                    <p>Java</p>
-                </div>
-                <div className="progress w-auto" role="progressbar" aria-label="Success striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                    <div className="progress-bar progress-bar-striped bg-success" style={{width: "50%"}}><strong><em>50%</em></strong></div>
-                </div>
-            </div>
-            {/* Python */}
-            <div className='place-self-auto pb-xl-5 pb-4'>
-                <div className='col'>
-                    <p>Python</p>
+                    <p>GIT</p>
                 </div>
                 <div className="progress w-auto" role="progressbar" aria-label="Success striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
                     <div className="progress-bar progress-bar-striped bg-success" style={{width: "60%"}}><strong><em>60%</em></strong></div>
+                </div>
+            </div>
+            {/* Linux */}
+            <div className='place-self-auto pb-xl-5 pb-4'>
+                <div className='col'>
+                    <p>Linux</p>
+                </div>
+                <div className="progress w-auto" role="progressbar" aria-label="Success striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                    <div className="progress-bar progress-bar-striped bg-success" style={{width: "75%"}}><strong><em>75%</em></strong></div>
                 </div>
             </div>
             </div>
