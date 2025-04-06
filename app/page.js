03d43ea84ac809d0fe3a1bc4ae6bb837630b47fe
shashankrp <shashankrp2@gmail.com>
2025-04-06 13:51:47 +0530

    fix: update link in Home page to point to About page

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
               <div className="d-flex flex-md-column justify-center w-auto px-md-5 row" >
                 <h1 className="pb-5 text-center font-monospace fw-bold"><em>Who am I?</em></h1>
               <p className="pl-4 w-5/6 break-words fs-4" style={{textAlign: 'justify'}}>Hello, I am <strong><em>Shashank</em></strong> and am a <strong><em>Software engineer</em></strong> with <strong><em>4+ years</em></strong> of experience in <strong><em>Full Stack Developer</em></strong> and currently working at <strong><em>Ellucian</em></strong>.</p>
-              <a className="btn btn-primary mt-4 text-center fw-bold" href="./skills">Know More</a>
+              <a className="btn btn-primary mt-4 text-center fw-bold" href="./about">Know More</a>
               </div>
             </div>
     </div>
