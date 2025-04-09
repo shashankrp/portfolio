50b427bf0fc479f3973d5d8f5f0ee4e06916f317
shashankrp <shashankrp2@gmail.com>
2025-04-09 13:03:11 +0530

    feat: add meta tags for improved SEO and portfolio description

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" webcrx="true">
       <head>
+        <title>
+          Shashank&apos;s Portfolio | Software Engineer | Full Stack Developer
+        </title>
       <meta name="robots" content="all" />
+      <meta name="description" content="Hello, I am Shashank and am a Software engineer with 4+ years of experience in Full Stack Developer and currently working at Ellucian." />
         {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"></link>
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></Script>
