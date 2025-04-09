38960298c2b88cbe5a75820fbcd1b08daa42b57d
shashankrp <shashankrp2@gmail.com>
2025-04-09 10:58:12 +0530

    feat: add Ghibli-style images and GIT best practices pages

---
+++ app/git_best_practices/page.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='container my-container'>
+      <div className='row pt-4'>
+        <div className='row pt-4'>
+          <div className="col">
+            <em><h3>GIT Best Practices</h3></em>
+          </div> 
+          <div className="col text-end">
+              <strong><em>9-Apr-2025</em></strong>
+          </div>
+        </div>
+        <div className='pt-4 blog-container'>
+          <p>
+            Git is a version control system that allows developers to track changes in their 
+            codebase. It is widely used in the software development industry and is an 
+            essential tool for any developer. Git is more powerful than other version 
+            control systems because it allows developers to work on multiple branches 
+            of code at the same time. This means that developers can work on new features 
+            or bug fixes without affecting the main codebase.
+          </p>
+          <p>
+            There are several best practices that developers should follow when using Git.
+            These best practices include using descriptive commit messages, keeping commits
+            small and focused, and using branches to organize code changes. Using
+            descriptive commit messages is important because it helps other developers
+            understand the changes that have been made to the codebase. Keeping commits
+            small and focused is also important because it makes it easier to review.
+          </p>
+          <p>
+            The best practices for Git are important because they help developers work more
+            efficiently and effectively. By following these best practices, developers would
+            be able to know the reasons behind the changes that have been made to the codebase. 
+            This would help them understand the code better and make it easier to work with.
+          </p>
+          <p>The 5 best practices for Git are as follows:</p>
+          <ol className='pt-1 pb-5 font-stretch-50%'>
+            <li>Use descriptive commit messages: When making a commit, it is important to use a
+              descriptive commit message that explains what changes have been made to the codebase.
+              This helps other developers understand the changes that have been made and makes it
+              easier to review the code.</li>
+            <li className='pt-2'>Keep commits small and focused: It is important to keep commits small and focused on a
+              specific change. This makes it easier to review the code and understand the changes that
+              have been made. It also makes it easier to revert changes if necessary.</li>
+            <li className='pt-2'>Use branches to organize code changes: Git allows developers to create branches to work on
+              new features or bug fixes without affecting the main codebase. This is a powerful feature of Git.</li>
+            <li className='pt-2'>Use pull requests to review code changes: When making changes to the codebase, it is important to use
+              pull requests to review the changes before they are merged into the main codebase. This helps ensure that
+              the changes are reviewed and approved by other developers before they are merged into the main codebase.</li>
+            <li className='pt-2'>
+              Use tags to mark important milestones: Git allows developers to create tags to mark important milestones in the codebase.
+            </li>
+          </ol>
+        </div>
+        <a href='./ghibli_style_images' className='background-none text-black text-decoration-none'>
+          <strong>Previous</strong><br/>
+          <p>Ghibli Style Images</p>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default page
