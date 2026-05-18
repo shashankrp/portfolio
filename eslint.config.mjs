7cfc31e9a738ed73e9a068ebd5ead807918bf0bd
shashankrp <shashankrp2@gmail.com>
2026-05-18 14:31:42 +0530

    updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 
@@ -13,19 +12,5 @@ const eslintConfig = defineConfig([
     "next-env.d.ts",
   ]),
 ]);
-=======
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
->>>>>>> 0fc4f2748cf4d18fdb8beee580ce2d932d263cdf
 
 export default eslintConfig;
